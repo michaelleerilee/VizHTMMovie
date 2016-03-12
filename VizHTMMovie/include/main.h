c3dc8537a986b3471156d50bafa4ae8910454212
Michael Rilee <mike@rilee.net>
2016-03-11 21:46:42 -0500

    Initial commit.

---
+++ VizHTMMovie/include/main.h
@@ -0,0 +1,22 @@
+/*
+ * main.h
+ *
+ *  Created on: Mar 11, 2016
+ *      Author: mrilee
+ */
+
+#ifndef MAIN_H_
+#define MAIN_H_
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+#include <Inventor/Qt/SoQt.h>
+#include <Inventor/Qt/viewers/SoQtExaminerViewer.h>
+
+#include <Inventor/nodes/SoSelection.h>
+
+#include <misc.h>
+
+#endif /* MAIN_H_ */
